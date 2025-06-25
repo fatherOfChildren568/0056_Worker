88ba6c9e46b76b49fbb733e177c1e8548202b547
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-06-26 01:07:25 +0700

    create function up and down salary

--- services/WorkerService.java
+++ services/WorkerService.java
@@ -3,6 +3,7 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
+import constants.SalaryStatus;
 import model.Salary;
 import model.Worker;
 import utility.GetLocalDate;
@@ -20,11 +21,32 @@ public class WorkerService {
         this.listWorkers = listWorkers;
     }
 
+    // up salary
     public void upSalary(int id, double upSalary) {
         for (Worker worker : listWorkers) {
             if (worker.getId() == id) {
-                worker.setSalary(worker.getSalary() + upSalary);
-                Salary salary = new Salary(worker.getSalary(), constants.SalaryStatus.UP, GetLocalDate.getDate());
+                // new salary
+                double newSalary = worker.getSalary() + upSalary;
+                worker.setSalary(newSalary);
+
+                // Ghi nhận lịch sử tăng lương
+                Salary salary = new Salary(newSalary, SalaryStatus.UP, GetLocalDate.getDate());
+                worker.getSalaryHistory().add(salary);
+            }
+        }
+    }
+
+    // down salary
+    public void downSalary(int id, double downSalary) {
+        for (Worker worker : listWorkers) {
+            if (worker.getId() == id) {
+                // count new salary
+                double newSalary = worker.getSalary() - downSalary;
+                if (newSalary < 0) {
+                    System.out.println("Error");
+                }
+                worker.setSalary(newSalary);
+                Salary salary = new Salary(worker.getSalary(), SalaryStatus.DOWN, GetLocalDate.getDate());
                 worker.getSalaryHistory().add(salary);
             }
         }
